5788f6fa12a5e34bcdddbc8848a64ac9d9c88546
public <publicmessage@protonmail.com>
2023-04-04 13:44:46 -0500

    use a custom startup.cs
    https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60?view=aspnetcore-7.0&tabs=visual-studio

--- Data/Usings.cs
+++ Data/Usings.cs
@@ -1,2 +1,1 @@
-﻿global using Microsoft.Extensions.Logging;
-global using System.Diagnostics;
+﻿
---
+++ Server/Startup.cs
@@ -0,0 +1,67 @@
+﻿using CallCenter.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+
+namespace CallCenter.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddDbContext<DefaultDbContextQuery>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                _ = app.UseExceptionHandler("/Error");
+                _ = app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+            // Configure the HTTP request pipeline.
+            //if (!app.Environment.IsDevelopment())
+            //{
+            //    _ = app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    _ = app.UseHsts();
+            //}
+
+            //app.UseHttpsRedirection();
+
+            //app.UseBlazorFrameworkFiles();
+            //app.UseStaticFiles();
+
+            //app.UseRouting();
+            //app.UseAuthorization();
+
+            //app.MapRazorPages();
+            //app.MapControllers(); 
+            //app.MapFallbackToFile("index.html");
+        }
+    }
+}
